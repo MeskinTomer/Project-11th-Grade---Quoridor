c54497d1e1538646abc80c2146e0b6dcd7a381d4
meskintomer <meskintomer@gmail.com>
2024-04-25 23:19:04 +0300

    Player and Block classes

--- Python FIles/Client.py
+++ Python FIles/Client.py
@@ -21,13 +21,72 @@ FILE_PATH_BLUE_PLAYER = os.path.join(FILE_PATH_IMAGES_FOLDER, 'player_blue.png')
 FILE_PATH_RED_PLAYER = os.path.join(FILE_PATH_IMAGES_FOLDER, 'player_red.png')
 
 
+class Player:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def get_position(self):
+        return self.x, self.y
+
+    def move_up(self):
+        self.y -= 80
+
+    def move_down(self):
+        self.y += 80
+
+    def move_right(self):
+        self.x += 80
+
+    def move_left(self):
+        self.x -= 80
+
+
+class Block:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.player = 0
+        self.up = 'clear'
+        self.down = 'clear'
+        self.right = 'clear'
+        self.left = 'clear'
+
+    def update_position(self, x, y):
+        self.x = x
+        self.y = y
+
+    def update_player(self, player):
+        self.player = player
+
+    def update_wall(self, side):
+        if side == 'up':
+            self.up = 'blocked'
+        elif side == 'down':
+            self.down = 'blocked'
+        elif side == 'right':
+            self.right = 'blocked'
+        elif side == 'left':
+            self.left = 'blocked'
+
+    def get_wall_status(self, side):
+        ret = ''
+        if side == 'up':
+            ret = self.up
+        elif side == 'down':
+            ret = self.down
+        elif side == 'right':
+            ret = self.right
+        elif side == 'left':
+            ret = self.left
+        return ret
+
+
 def main():
     pygame.init()
     size = (WINDOW_WIDTH, WINDOW_HEIGHT)
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Quoridor")
-
-    clock = pygame.time.Clock()
 
     board = pygame.image.load(FILE_PATH_BOARD)
     screen.blit(board, (0, 0))
@@ -47,7 +106,6 @@ def main():
             if event.type == pygame.QUIT:
                 finish = True
         pygame.display.flip()
-        clock.tick(REFRESH_RATE)
 
     pygame.quit()
 
--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Project-11th-Grade---Quoridor
 My final project for 11th grade, Cyber - The game Quoridor.
 Written in Python, using Socket communication.
-The game will consist of 2 players, racing to reach the ooposite side of the board. The player can either move once, or place a wall during thier turn. A wall can't be placed, if it's position entirly blocks the way of a player to reach the other side.
+The game will consist of 2 players, racing to reach the opposite side of the board. The player can either move once, or place a wall during their turn. A wall can't be placed, if it's position entirely blocks the way of a player to reach the other side.
 
 # Planning
 Server - 
 No threads/select
 Sends Timer starts messages
-Responssible for switching turns
-Has a timeout of 1.5 minuts for a player's turn to resign
+Responsible for switching turns
+Has a timeout of 1.5 minutes for a player's turn to resign
 
 User -
-Has Select meskanism for user input and Pygame screen
-Starts own timer of it's/other's turn. - 1 minute timer.
-Has a PathFinding meskanism for checking viability of wall placement.
-Has a User Input meskanism
+Has Select mechanism for user input and Pygame screen
+Starts own timer of it's/other's turn. - 1-minute timer.
+Has a PathFinding mechanism for checking viability of wall placement.
+Has a User Input mechanism
 
 Both -
 Have the board as a 2D array which consists each cell as an Object.
